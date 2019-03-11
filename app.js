ebc8a27232230c563c89635fab93f0007dcee452
Sumeet <sumeetkhirwal@gmail.com>
2019-03-11 13:08:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ const app = express();
 module.exports = require('./config/express')(app, config);
 
 app.listen(config.port, () => {
-  console.log('Express server listening on port ' + config.port);
+  console.log('Express server listening on port :' + config.port);
 });
 
